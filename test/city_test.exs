ef0bb19e7a214307acb1f734d64aa6b657cb1310
Piotr Szeremeta <piotr.szeremeta@gmail.com>
2016-08-05 23:09:00 +0200

    Test refactor
    We've moved setup methods out of the way.

--- test/city_test.exs
+++ test/city_test.exs
@@ -3,22 +3,35 @@ defmodule Orange.CityTest do
   alias Orange.City
 
   describe "With started City register" do
-    setup(context) do
-      {:ok, city_reg_pid} = City.start_link
-      {:ok, Map.put(context, :pid, city_reg_pid)} 
-    end
+    setup [:start_city_server]
 
     test "City creates new city and assigns a prefix to it", %{pid: pid} do
       city_name = "Krakow"
       assert {:ok, prefix} = City.register(pid, city_name)
       assert is_number(prefix)
     end
+  end
 
-    test "City finds city by its prefix", %{pid: pid} do
-      city_name = "Krakow"
-      {:ok, prefix} = City.register(pid, city_name)
+  describe "With registered city" do
+    setup [:start_city_server, :register_city]
+
+    test "City finds city by its prefix", %{pid: pid, city_name: city_name, prefix: prefix} do
       assert City.find(pid, prefix) == city_name
     end
+
+    test "City registers new number", %{pid: pid, city_name: city_name, prefix: prefix} do
+    end
+  end
+
+  def register_city(context) do
+    city_name = "Krakow"
+    {:ok, prefix} = City.register(context.pid, city_name)
+    {:ok, Map.merge(context, %{city_name: city_name, prefix: prefix})}
+  end
+
+  def start_city_server(context) do
+    {:ok, city_reg_pid} = City.start_link
+    {:ok, Map.put(context, :pid, city_reg_pid)}
   end
 
 end
